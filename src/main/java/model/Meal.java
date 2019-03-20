d1083552a187d9cfb003086d6b5391b3e2ce32e5
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-03-20 14:55:56 +0100

    Create meal class (not finished)

--- src/main/java/model/Meal.java
+++ src/main/java/model/Meal.java
@@ -2,5 +2,20 @@ package model;
 
 public class Meal {
 
+    private long MealId;
+    private String name;
+    private String address;
+    private double price;
+    private int quantity;
+    private String MealDescription;
+
+    public Meal(String name, String address, double price, int quantity, String Mealdescriptionn) {
+        this.name = name;
+        this.address = address;
+        this.price = price;
+        this.quantity = quantity;
+        this.MealDescription = MealDescription;
+        MealId++;
+    }
 
 }
