2ac6ae067b79355a8d9c5d97da80bbe24b0fa80c
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-03-22 14:08:49 +0100

    Reading the meals from the database

---
+++ src/main/java/ReadMealsJdbcExample.java
@@ -0,0 +1,33 @@
+import repository.DataSourceUtil;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReadMealsJdbcExample {
+
+
+    public static void main(String... args) throws SQLException {
+
+        DataSource dataSource = DataSourceUtil.getDataSourceInPath();
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("select * from meals");
+
+        while (rs.next()) {
+
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            int quantity = rs.getInt("quantity");
+            double price = rs.getDouble("price");
+
+            System.out.println(id + ", dish: " + name + ", Qty: " + quantity + ", Price: " + price);
+        }
+
+        rs.close();
+        statement.close();
+        connection.close();
+    }
+}
