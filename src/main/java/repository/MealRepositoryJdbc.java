18e3ac08b64da4389c11edadc9af9ee5bcf9cd58
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-03-22 14:07:34 +0100

    Implement several methods with JDBC

--- src/main/java/repository/MealRepositoryJdbc.java
+++ src/main/java/repository/MealRepositoryJdbc.java
@@ -1,10 +1,14 @@
 package repository;
 
+import model.Meal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
 
 /**
  * Stores meals in a database
@@ -14,11 +18,56 @@ public class MealRepositoryJdbc {
 
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
+
     public MealRepositoryJdbc() {
         DataSource dataSource = DataSourceUtil.getDataSourceInPath();
         this.jdbcTemplate = new JdbcTemplate(dataSource);
 
+    }
+
+    public void add(Meal meal){
+        jdbcTemplate.update("insert into meals (name, quantity, price)" +
+                " values ('" + meal.getName() + "','"
+                + meal.getQuantity() + "','"+ meal.getPrice()+"')");
+    }
+
+    public void update(Meal meal){
+
+        String sql = "update meals set " +
+                "name = '" + meal.getName() + "', " +
+                "quantity = '" + meal.getQuantity() + "', " +
+                "price = " + meal.getPrice() + " " +
+                "where id = " + meal.getId();
+
+        //System.out.println("SQL: " + sql);
+        jdbcTemplate.update(sql);
+    }
+
+
+
+    public Meal getById(long id){
+        return jdbcTemplate.queryForObject("select * from meals where id = " + id,
+                (rs1, rowNum) -> getMeal(rs1));
+    }
+
+
+    private Meal getMeal(ResultSet rs) throws SQLException{
+
+        long id = rs.getLong("id");
+        String name = rs.getString("name");
+        int quantity = rs.getInt("quantity");
+        double price = rs.getDouble("price");
+
+        Meal meal = new Meal(name, price, quantity);
+        meal.setId(id);
+
+        return meal;
+    }
+
+    public Collection<Meal> getAllMeals(){
+
+        return jdbcTemplate.query("select * from meals",
+                (rs1, rowNum) -> getMeal(rs1));
     }
 
     
