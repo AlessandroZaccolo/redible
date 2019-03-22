e1bc6333c25a7c0b9912764a4be1e0c093bce9d0
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-03-22 14:08:28 +0100

    Implement getMealById and GetAllMeals methods

--- src/main/java/service/MealService.java
+++ src/main/java/service/MealService.java
@@ -1,8 +1,11 @@
 package service;
 
+import model.Meal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.MealRepositoryJdbc;
+
+import java.util.Collection;
 
 @Service
 public class MealService {
@@ -13,5 +16,15 @@ public class MealService {
     @Autowired
     public MealService(MealRepositoryJdbc mealRepo) { this.mealRepo = mealRepo; }
 
+
+    public Meal getMealById(long id){
+
+        return mealRepo.getById(id);
+    }
+
+    public Collection<Meal> getAllMeals(){
+
+        return mealRepo.getAllMeals();
+    }
 
 }
