611f14381d319cbf5eb81c9786c5127ef076deeb
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-04-03 17:34:24 +0200

    Start repositoryMongo

--- src/main/java/com/redible/Application.java
+++ src/main/java/com/redible/Application.java
@@ -1,8 +1,13 @@
 package com.redible;
 
 
+import com.redible.repository.MealRepository;
+import com.redible.repository.MealRepositoryMongo;
+import com.redible.service.MealService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -11,6 +16,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner test(
+            MealService mealService,
+            MealRepositoryMongo mealRepository){
+        return args -> {
 
+        };
+    }
 
 }
