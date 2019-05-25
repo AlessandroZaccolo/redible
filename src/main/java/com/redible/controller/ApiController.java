977f9e31b4a88fb3a17fdd8036dd5e05113d2bff
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-05-25 17:08:17 +0200

    Create sampleApi

---
+++ src/main/java/com/redible/controller/ApiController.java
@@ -0,0 +1,42 @@
+package com.redible.controller;
+
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.redible.model.Meal;
+import com.redible.service.MealService;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(path= "/api")
+public class ApiController {
+
+    private MongoCollection<Document> meals;
+
+    @Autowired
+    public ApiController(@Value("${MONGO_URI}") String mongoUri) {
+
+        final MongoClient mongoClient = MongoClients.create(mongoUri);
+        final MongoDatabase database = mongoClient.getDatabase("redible");
+        this.meals = database.getCollection("meals");
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from sample app";
+    }
+
+
+
+
+}
